a7d34f3f6c7556f2b5b1dafff3cf088b1eba2991
d0422 <rlfehd2013@naver.com>
2023-07-09 00:15:36 +0900

    refactor: login service분리

--- src/controller/LoginController.ts
+++ src/controller/LoginController.ts
@@ -1,30 +1,18 @@
 import { Request, Response } from 'express';
-import { UserDAO } from '../DAO/LoginDAO';
-import { JWTService } from '../service/JWTService';
-import { createHash } from 'crypto';
-import ErrorStatus from '../utils/ErrorStatus';
+import { UserService } from '../service/UserService';
 
 export class LoginController {
   static async login(req: Request, res: Response) {
     const { id, pw } = req.body;
-    const result = await UserDAO.getUser(id);
-    const compareHash = createHash('sha512')
-      .update(pw + result.salt)
-      .digest('hex');
-
-    if (result.id === id && result.pw === compareHash) {
-      const { accessToken, refreshToken } = JWTService.make(result.id);
-      const response = {
-        status: 200,
-        message: '로그인 성공',
-        data: {
-          accessToken: accessToken,
-          refreshToken: refreshToken,
-        },
-      };
-      res.json(response);
-    } else {
-      throw new ErrorStatus('로그인 실패', 401);
-    }
+    const { accessToken, refreshToken } = await UserService.login(id, pw);
+    const response = {
+      status: 200,
+      message: '로그인 성공',
+      data: {
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+      },
+    };
+    res.json(response);
   }
 }
--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,10 +1,22 @@
+import { createHash } from 'crypto';
 import { UserDAO } from '../DAO/LoginDAO';
 import { JWTService } from './JWTService';
+import ErrorStatus from '../utils/ErrorStatus';
 
 export class UserService {
   static async getUserInfo(token) {
     const userId = await JWTService.verify(token);
     const result = await UserDAO.getUser(userId.id);
     return result;
+  }
+  static async login(id: string, pw: string) {
+    const result = await UserDAO.getUser(id);
+    const compareHash = createHash('sha512')
+      .update(pw + result.salt)
+      .digest('hex');
+
+    if (result.id === id && result.pw === compareHash)
+      return JWTService.make(result.id);
+    throw new ErrorStatus('로그인 실패', 401);
   }
 }
