8688c6e8cd829bea68adca586b3abfe60ce1bd2a
d0422 <rlfehd2013@naver.com>
2023-07-09 00:12:25 +0900

    feat: UserService getUserInfo 추가

--- src/controller/MypageController.ts
+++ src/controller/MypageController.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from 'express';
-import { JWTService } from '../service/JWTService';
-import { UserDAO } from '../DAO/LoginDAO';
+import { UserService } from '../service/UserService';
 
 export class MyPageController {
   static async get(req: Request, res: Response) {
-    const userId = await JWTService.verify(req.headers.authorization);
-    const result = await UserDAO.getUser(userId.id);
-    res.status(200).json({ name: result.name, age: result.age });
+    const token = req.headers.authorization;
+    const user = await UserService.getUserInfo(token);
+    res.status(200).json({ name: user.name, age: user.age });
   }
 }
--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,1 +1,10 @@
-class UserService {}
+import { UserDAO } from '../DAO/LoginDAO';
+import { JWTService } from './JWTService';
+
+export class UserService {
+  static async getUserInfo(token) {
+    const userId = await JWTService.verify(token);
+    const result = await UserDAO.getUser(userId.id);
+    return result;
+  }
+}
